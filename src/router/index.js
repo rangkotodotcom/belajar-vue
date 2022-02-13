aae17f83667b7371a215c9f9b6d9eb441f45417d
rangkotodotcom <softj4m98@gmail.com>
2022-02-13 12:43:30 +0700

    Commit satu

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Blog from "../views/Blog.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/blog",
+    name: "Blog",
+    component: Blog,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
